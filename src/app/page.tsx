6b56914b2584868b22ea452db0b7796b2bf38457
yash <jewalkaryash@gmail.com>
2025-01-11 19:14:21 +0530

    modifies css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import Slider from "@/Components/Slider";
 export default function Home() {
   return (
    <div>
-      <div className="w-screen h-screen flex justify-center items-center text-[72px] bg-black">
+      <div className="w-screen h-screen flex justify-center items-center text-[72px] bg-white text-black">
 
         <p>Here it goes</p>
 
       </div>
         <Slider/>
-        <div className="w-screen h-screen flex justify-center items-center text-[72px] bg-black">
+        <div className="w-screen h-screen flex justify-center items-center text-[72px] bg-white text-black">
 
         <p>Here it Ends</p>
 
